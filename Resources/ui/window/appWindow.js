f048763b1b5006672f917975f95237e0baa810c2
Alejandro FCarrera <afcarrera@fi.upm.es>
2014-02-14 10:26:10 +0100

    [appWindow] 3.2.1GA Compatibility and Global Variables

--- Resources/ui/window/appWindow.js
+++ Resources/ui/window/appWindow.js
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2014 by Center Open Middleware. All Rights Reserved.
- * Titanium Appcelerator 3.2.0GA
+ * Titanium Appcelerator 3.2.1GA
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  *
@@ -9,11 +9,6 @@
 "use strict";
 
 // Libraries
-/** It shows a message on the console log.
- * @param {String} msg It will be shown on console.
- * @param {String} newType message level to show on console.
- * @param {Object} error javascript exception object.
- * @param {Object} extra details about error. */
 Ti.App.Log = function(msg, newType, error, extra){
     var type = 'Error', _self = {
         'name': 'AppBaseLog',
@@ -32,9 +27,6 @@ Ti.App.Log = function(msg, newType, error, extra){
     Ti.API[type](_self);
     return _self;
 };
-Ti.App.FontAwesome4 = require('fonts/FontAwesome4');
-Ti.App.FontAwesome3 = require('fonts/FontAwesome');
-Ti.App.API = require('lib/API');
 Ti.App.mergeObject = function (obj1, obj2){
     var result = null, key;
     if (obj1 !== null && obj2 !== null){
@@ -45,33 +37,29 @@ Ti.App.mergeObject = function (obj1, obj2){
     }
     return result;
 };
+Ti.App.FontAwesome4 = require('fonts/FontAwesome4');
+Ti.App.FontAwesome3 = require('fonts/FontAwesome');
+Ti.App.API = require('lib/API');
 
-// Global Variables
-Ti.App.isApple = (Ti.Platform.getOsname() === 'ipad' || Ti.Platform.getOsname() === 'iphone');
-Ti.App.isRetina = (Ti.App.isApple && Ti.Platform.displayCaps.getDpi() === 260) ? true : false;
-Ti.App.platformHeight = Ti.Platform.displayCaps.getPlatformHeight();
-Ti.App.platformWidth = Ti.Platform.displayCaps.getPlatformWidth();
+// Login HTTP Basic Cache Deleted
+Ti.App.Properties.removeProperty('cookie_csrftoken');
+Ti.App.Properties.removeProperty('cookie_sessionid');
+Ti.App.Properties.removeProperty('cookie_oilsid');
+
+// Quick Start
+if(Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,'composition').exists()){
+    Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,'composition').createDirectory();
+}
+if(Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,'component').exists()){
+    Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,'component').createDirectory();
+}
 
 var appWindow = (function () {
 
     var theme = require('ui/style/appWindowStyle'),
      _self = Ti.UI.createWindow(theme), loginView;
 
-    // Quick Start
-    if(Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,'composition').exists()){
-        Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,'composition').createDirectory();
-    }
-    if(Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,'component').exists()){
-        Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,'component').createDirectory();
-	}
-
-	// Login HTTP Basic Cache Deleted
-	Ti.App.Properties.removeProperty('cookie_csrftoken');
-	Ti.App.Properties.removeProperty('cookie_sessionid');
-	Ti.App.Properties.removeProperty('cookie_oilsid');
-
 	_self.showMainView = function showMainView(data) {
-
 		_self.remove(loginView.view);
 		loginView.destroy();
 		loginView = null;
@@ -79,7 +67,6 @@ var appWindow = (function () {
 		_self.add(mainView);
 		mainView = null;
 		delete _self.showMainView;
-
 	};
 
 	loginView = require('ui/view/loginView')(_self);
@@ -90,4 +77,3 @@ var appWindow = (function () {
 }());
 
 module.exports = appWindow;
-
