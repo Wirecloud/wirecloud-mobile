4183425e34ec8bf69e6a7d33460647932c422dce
Alejandro FCarrera <afcarrera@fi.upm.es>
2014-03-15 19:45:14 +0100

    [mainView] Refactor MainView and Load new Configuration Compositions [appBase] Update 3.2.2 Headers

--- Resources/ui/style/mainViewStyle.js
+++ Resources/ui/style/mainViewStyle.js
@@ -11,13 +11,17 @@
 var mainViewStyle = (function() {
 
     var _self = {},
-    heightView = (Ti.App.isApple) ? Ti.App.platformHeight - 20 : Ti.App.platformHeight;
+    heightView = (Ti.App.API.HW.System.isApple()) ?
+                  Ti.App.API.HW.System.getPlatformHeight() - 20 :
+                  Ti.App.API.HW.System.getPlatformHeight(),
+    rowHeight =  (Ti.App.API.HW.System.isApple()) ? 44 : '48dp',
+    rowFontSize = (Ti.App.API.HW.System.isApple()) ? '20' : '18dp';
 
     _self.view = {
-        top: (Ti.App.isApple) ? 20 : 0,
+        top: (Ti.App.API.HW.System.isApple()) ? 20 : 0,
         left: 0,
         height: heightView,
-        width: Ti.App.platformWidth,
+        width: Ti.App.API.HW.System.getPlatformWidth(),
         backgroundColor: '#1F3346'
     };
 
@@ -30,22 +34,22 @@ var mainViewStyle = (function() {
     };
 
     _self.topBar = {
-        top: (Ti.App.isApple) ? 1 : 0,
+        top: (Ti.App.API.HW.System.isApple()) ? 1 : 0,
         left: 0,
-        height: (Ti.App.isApple) ? 44 : '48dp',
+        height: rowHeight,
         width: _self.view.width,
         backgroundColor: '#EAEAEA'
     };
 
     _self.button = {
-        height: (Ti.App.isApple) ? 30 : '40dp',
-        width: (Ti.App.isApple) ? 30 : '40dp',
-        top: (Ti.App.isApple) ? 7 : '4dp',
+        height: (Ti.App.API.HW.System.isApple()) ? 30 : '40dp',
+        width: (Ti.App.API.HW.System.isApple()) ? 30 : '40dp',
+        top: (Ti.App.API.HW.System.isApple()) ? 7 : '4dp',
         font : {
-            fontSize : (Ti.App.isApple) ? '28' : '18dp',
+            fontSize : (Ti.App.API.HW.System.isApple()) ? '28' : '18dp',
             fontFamily : Ti.App.FontAwesome4.getFontFamily()
         },
-        color: (Ti.App.isApple) ? '#34AADC' : '#C1C1C1',
+        color: (Ti.App.API.HW.System.isApple()) ? '#34AADC' : '#C1C1C1',
         shadowColor: '#aaa',
         shadowOffset: {x:0, y:0},
         shadowRadius: 3,
@@ -53,20 +57,132 @@ var mainViewStyle = (function() {
     };
 
     _self.labelButton = {
-        height: (Ti.App.isApple) ? 30 : '40dp',
-        top: (Ti.App.isApple) ? 7 : '4dp',
+        height: (Ti.App.API.HW.System.isApple()) ? 30 : '40dp',
+        top: (Ti.App.API.HW.System.isApple()) ? 7 : '4dp',
         font : {
-            fontSize : (Ti.App.isApple) ? '20' : '18dp',
+            fontSize : (Ti.App.API.HW.System.isApple()) ? '20' : '18dp',
             fontFamily : 'Default'
         },
-        color: (Ti.App.isApple) ? '#34AADC' : '#C1C1C1',
+        color: (Ti.App.API.HW.System.isApple()) ? '#34AADC' : '#C1C1C1',
         shadowColor: '#aaa',
         shadowOffset: {x:0, y:0},
         shadowRadius: 3,
         textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER
     };
 
-    if(Ti.App.isRetina){
+    _self.leftListView = {
+        left: 0,
+        height: _self.view.height - (_self.topBar.top + _self.topBar.height),
+        width: _self.view.width * 0.3,
+        top: _self.topBar.top + _self.topBar.height,
+        separatorColor: '#8EAECC',
+        separatorInsets:{
+            right: 20
+        },
+        backgroundColor: '#3F566D'
+    };
+
+    _self.leftListViewTemplate = {
+        childTemplates: [
+            {
+                type: 'Ti.UI.Label',
+                bindId: 'icon',
+                properties: {
+                    layout: 'horizontal',
+                    width:  rowHeight - 10,
+                    height:  rowHeight - 10,
+                    borderRadius: '10',
+                    borderWidth: '1',
+                    borderColor: '#0E171F',
+                    backgroundColor: '#D4D4D4',
+                    color: '#149F9D',
+                    left: 10,
+                    font: {
+                        fontFamily: 'FontAwesome',
+                        fontSize: rowFontSize
+                    },
+                    textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER
+                }
+            }, {
+                type: 'Ti.UI.Label',
+                bindId: 'title',
+                properties: {
+                    width:  _self.leftListView.width - (20 + (rowHeight - 10)),
+                    layout: 'horizontal',
+                    color: '#FFFFFF',
+                    font: {
+                        fontFamily:'Default',
+                        fontSize: rowFontSize
+                    },
+                    left: 20 + (rowHeight - 10),
+                    height: rowHeight,
+                    textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT
+                }
+            }
+        ],
+        properties: {
+            selectedBackgroundColor: '#4F6C88',
+            backgroundColor: '#3F566D'
+        }
+    };
+
+    _self.leftListViewNoWorkspaces = {
+        left: 0,
+        height: _self.leftListView.height,
+        top: _self.leftListView.top,
+        width: _self.leftListView.width,
+        color: '#3F566D',
+        font: {
+            fontFamily:'Default',
+            fontSize: rowFontSize
+        },
+        text: Ti.Locale.getString('LABEL_NOWORKSPACE'),
+        textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER
+    };
+
+    _self.leftShadowView = {
+        left: _self.leftListView.width,
+        height: _self.leftListView.height,
+        width: 15,
+        top: _self.leftListView.top,
+        backgroundGradient: {
+            type: 'linear',
+            startPoint: { x: 0, y: 0 },
+            endPoint: { x: 15, y: 0 },
+            colors: [
+                { color: '#3F566D', offset: 0.0 },
+                { color: '#4F6C88', offset: 1.0 }
+            ]
+        },
+        opacity: 1
+    };
+
+    _self.rightView = {
+        left: _self.leftListView.width,
+        width: _self.view.width * 0.7,
+        height: _self.leftListView.height,
+        top: _self.leftListView.top,
+        backgroundColor: '#4F6C88'
+    };
+
+    _self.leftHeaderListView = {
+        left: 0,
+        backgroundColor: '#3F566D',
+        width: _self.leftListView.width,
+        height: rowHeight,
+        font : {
+            fontSize : rowFontSize,
+            fontFamily : 'Default'
+        },
+        color: (Ti.App.API.HW.System.isApple()) ? '#34AADC' : '#C1C1C1',
+        shadowColor: '#aaa',
+        shadowOffset: {x:0, y:0},
+        shadowRadius: 3,
+        textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+        text: 'Compositions'
+    };
+
+    if(Ti.App.API.HW.System.isRetina()){
         _self.topBar.height *= 2;
         _self.button.height *= 2;
         _self.button.width *= 2;
@@ -75,6 +191,7 @@ var mainViewStyle = (function() {
         _self.labelButton.height *= 2;
         _self.labelButton.left *= 2;
         _self.labelButton.top *= 2;
+        _self.leftHeaderListView.height *= 2;
     }
 
     return _self;
--- Resources/ui/view/mainView.js
+++ Resources/ui/view/mainView.js
@@ -1,6 +1,7 @@
+
 /**
  * Copyright (c) 2014 by Center Open Middleware. All Rights Reserved.
- * Titanium Appcelerator 3.2.0GA
+ * Titanium Appcelerator 3.2.2GA
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  *
@@ -8,27 +9,38 @@
 
 "use strict";
 
-var mainView = function mainView() {
+var mainView = function mainView(parentWindow) {
 
     // Create References
     var theme = require('ui/style/mainViewStyle'),
     topBar = Ti.UI.createView(theme.topBar),
     buttonLogout = Ti.UI.createLabel(Ti.App.mergeObject(theme.button,{
-        left: (Ti.App.isApple) ? 14 : '8dp',
+        left: (Ti.App.API.HW.System.isApple()) ? 14 : '8dp',
         text: Ti.App.FontAwesome4.getCharCode('fa-sign-out')
     })),
     buttonStore = Ti.UI.createLabel(Ti.App.mergeObject(theme.button,{
-        left: (Ti.App.isApple) ? 14 : '8dp',
+        left: (Ti.App.API.HW.System.isApple()) ? 14 : '8dp',
         text: Ti.App.FontAwesome4.getCharCode('fa-cloud')
     })),
-    _self = Ti.UI.createView(theme.view);
+    leftView = Ti.UI.createListView(Ti.App.mergeObject(theme.leftListView, {
+        templates: {
+            'template': theme.leftListViewTemplate
+        },
+        defaultItemTemplate: 'template'
+    })),
+    shadowView = Ti.UI.createView(theme.leftShadowView),
+    rightView = Ti.UI.createView(theme.rightView),
+    compositions = {},
+    _self = {
+        view : Ti.UI.createView(theme.view)
+    };
 
     // Create Navigation Bar
-    if(Ti.App.isApple){
-        _self.add(Ti.UI.createView(theme.line));
+    if(Ti.App.API.HW.System.isApple()){
+        _self.view.add(Ti.UI.createView(theme.line));
     }
     topBar.add(buttonLogout);
-    buttonStore.setLeft(topBar.getWidth() - buttonStore.getLeft() - buttonStore.getWidth() - 50);
+    buttonStore.setLeft(topBar.getWidth() - (buttonStore.getLeft() + buttonStore.getWidth() + 50));
     topBar.add(buttonStore);
     topBar.add(Ti.UI.createLabel(Ti.App.mergeObject(theme.labelButton,{
         left: buttonLogout.getLeft() + buttonLogout.getWidth(),
@@ -38,9 +50,88 @@ var mainView = function mainView() {
         left: buttonStore.getLeft() + buttonStore.getWidth(),
         text: ' Store'
     })));
-    _self.add(topBar);
+    _self.view.add(topBar);
 
-	return _self;
+    // Create Left Header ListView
+    leftView.setHeaderView(Ti.UI.createLabel(theme.leftHeaderListView));
+
+    // Add Views
+    _self.view.add(rightView);
+    _self.view.add(shadowView);
+    _self.view.add(leftView);
+
+    // Bind click Logout Button
+    _self.clickLogoutButton = function clickLogoutButton() {
+        parentWindow.showLoginView();
+    };
+    buttonLogout.addEventListener('singletap', _self.clickLogoutButton);
+
+    // Bind click Store Button
+    _self.clickStoreButton = function clickStoreButton() {
+        parentWindow.showStoreView();
+    };
+    buttonStore.addEventListener('singletap', _self.clickStoreButton);
+
+    // Bind click ListView
+    _self.clickRowListView = function clickRowListView(e) {
+        Ti.App.API.HW.Network.getWorkspaceInfo(function(result){
+            if(result === 'Error getWorkspaceInfo'){
+                alert(result);
+            }
+            else{
+                parentWindow.showCompositionView(result);
+            }
+        });
+    };
+    leftView.addEventListener('itemclick', _self.clickRowListView);
+
+    // Create Connection to fill ListView
+    _self.reloadTable = function reloadTable(){
+        var compFolder = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, 'composition').getDirectoryListing(), i;
+        if(compFolder.length === 0){
+            leftView.setTouchEnabled(false);
+            leftView.add(Ti.UI.createView(theme.leftListViewNoWorkspaces));
+        }
+        else{
+            var compositions = Ti.UI.createListSection();
+            var rows = [];
+            for(i = 0; i < compFolder.length; i++){
+                var metadataComp = JSON.parse(Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,
+                    'composition/' + compFolder[i] + '/', '.metadata').read().toString());
+                rows.push({
+                    id: {
+                        text: compFolder[i]
+                    },
+                    title: {
+                        text: metadataComp.name
+                    },
+                    icon: {
+                        text : (metadataComp.icon) ? Ti.App.FontAwesome4.getCharCode(metadataComp.icon) :
+                               Ti.App.FontAwesome4.getCharCode('fa-columns')
+                    }
+                });
+            }
+            compositions.setItems(rows);
+            leftView.setSections([compositions]);
+            compositions = null;
+            rows = null;
+        }
+    };
+
+    // Load Workspaces on ListView
+    _self.reloadTable();
+
+    // Destroy MainView
+    _self.destroy = function destroy(){
+        _self.view.remove(rightView);
+        _self.view.remove(shadowView);
+        _self.view.remove(leftView);
+        leftView.removeEventListener('itemclick', _self.clickRowListView);
+        buttonLogout.removeEventListener('singletap', _self.clickLogoutButton);
+        buttonStore.removeEventListener('singletap', _self.clickStoreButton);
+    };
+
+    return _self;
 
 };
 
