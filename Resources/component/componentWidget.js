f10ef15629c532bf0a9c25be9cb6fb6aee1ef0ce
Carlos Blanco <cblanco@conwet.com>
2014-07-25 10:22:47 +0200

    Update Component Widget. Setting viewID in mashupPlatform and bridge

--- Resources/component/componentWidget.js
+++ Resources/component/componentWidget.js
@@ -3,35 +3,28 @@
 'use strict';
 
 function widgetGeneric(dim, parameters, idWidget) {
-
 	var _isApple = Yaast.API.HW.System.isApple();
-	var _self;
-	if(parameters.name.indexOf("map-viewer") !== -1){
+	var _self, res;
+	if(parameters.name.indexOf("map-viewer") !== -1) {
 		var _widgetMapClass = require("ui/widgets/widgetMap");
 		_self = _widgetMapClass({height: dim.height, width: dim.width}, idWidget);
 		_widgetMapClass = null;
-	}
-	else{
+	} else {
 		var _routeHTML = Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), 'widgets/'+parameters.uri+'/TIWebView');
 		_routeHTML = _routeHTML.read().toString();
 		_routeHTML = Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), 'widgets/'+parameters.uri+'/'+_routeHTML);
 		var _textHTML = _routeHTML.read().toString();
-		if(_textHTML.search('mashupPlatform.js') < 0){
-            var _fileBridge = Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), 'widgets/'+parameters.uri+'/APIBridge.js');
-			var _fileMashupPlatform = Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), 'widgets/'+parameters.uri+'/mashupPlatform.js');
-			var _textMashupOriginal = Ti.Filesystem.getFile(Ti.Filesystem.getResourcesDirectory(), 'component/mashupPlatform.lib').read().toString();
-            var _textBridgeOriginal = Ti.Filesystem.getFile(Ti.Filesystem.getResourcesDirectory(), 'component/APIBridgeJS.lib').read().toString();
 
-            // Set Android/iOS var in the bridge. appleOS
+		var _fileBridge = Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), 'widgets/'+parameters.uri+'/APIBridge.js');
+		var _textBridgeOriginal = Ti.Filesystem.getFile(Ti.Filesystem.getResourcesDirectory(), 'component/APIBridgeJS.lib').read().toString();
+        var _fileMashupPlatform = Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), 'widgets/'+parameters.uri+'/mashupPlatform.js');
+        var _textMashupOriginal = Ti.Filesystem.getFile(Ti.Filesystem.getResourcesDirectory(), 'component/mashupPlatform.lib').read().toString();
+
+		if(_textHTML.search('APIBridge.js') < 0) {
+            // Set Android/iOS var in the bridge. appleOS. ONLY first time for this widget
             var res = _textBridgeOriginal.split("// ChangeMeYaaST!! appleOS bool");
             _textBridgeOriginal = res[0] + "var appleOS = " + _isApple + ";" + res[1];
 
-            _fileMashupPlatform.write(_textMashupOriginal, false);
-            _fileBridge.write(_textBridgeOriginal, false);
-			_textMashupOriginal = null;
-			_textBridgeOriginal = null;
-			_fileMashupPlatform = null;
-			_fileBridge = null;
 			var _routeMashup = '\t\t<script type="text/javascript" src="mashupPlatform.js"></script>';
 			var _routeAPI = '\t\t<script type="text/javascript" src="APIBridge.js"></script>';
 			_routeMashup = '<head>\n' + _routeMashup + '\n' + _routeAPI;
@@ -41,6 +34,19 @@ function widgetGeneric(dim, parameters, idWidget) {
 			_routeAPI = null;
 		}
 
+        // Set the new widget ID in all cases for the current version of Wirecloud4Tablet
+        res = _textBridgeOriginal.split("// ChangeMeYaaST!! MagicID");
+        _textBridgeOriginal = res[0] + "// ChangeMeYaaST!! MagicID\n" + "var id = " + idWidget + ";\n" + "// ChangeMeYaaST!! MagicID" + res[2];
+        res = _textMashupOriginal.split("// ChangeMeYaaST!! MagicID");
+        _textMashupOriginal = res[0] + "// ChangeMeYaaST!! MagicID\n" + "var id = " + idWidget + ";\n" + "// ChangeMeYaaST!! MagicID" + res[2];
+
+        _fileMashupPlatform.write(_textMashupOriginal, false);
+        _fileBridge.write(_textBridgeOriginal, false);
+
+        _textBridgeOriginal = null;
+        _fileBridge = null;
+        _textMashupOriginal = null;
+        _fileMashupPlatform = null;
 
 		_textHTML = null;
 		_self = Ti.UI.createWebView({
