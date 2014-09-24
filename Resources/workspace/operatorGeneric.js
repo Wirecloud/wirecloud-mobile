7adb0d2f9350ac53fe0141899f65e2c67ceb6666
Carlos Blanco <cblanco@conwet.com>
2014-09-24 15:43:38 +0200

    Clena Operator and widget generic

--- Resources/workspace/operatorGeneric.js
+++ Resources/workspace/operatorGeneric.js
@@ -4,31 +4,26 @@ function operatorGeneric(parameters, idOperator, userName) {
 
 	var _isApple = (Ti.Platform.osname == 'ipad');
 	var _self;
-	/*if (parameters.name.indexOf("GPS-Simulator") !== -1) {
-		var _operatorGPSClass = require("ui/widgets/operatorGPS");
-		_self = _operatorGPSClass(idOperator);
-	} else {*/
-		var _routeHTML = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, userName + '/operators/' + parameters.uri + '/index.html');
-		_self = Ti.UI.createWebView({
-			url : _routeHTML.nativePath,
-			width : 0,
-			height : 0,
-			top : 0,
-			left : 0,
-			flag : true,
-			visible : false
-		});
-		_self.funPlatformInfo = function funPlatformInfo() {
-			_self.evalJS("MashupPlatform.setPlatformInfo(" + idOperator + "," + "'operator'" + ");");
-		};
-		_self.clearObject = function clearObject() {
-			_self.removeEventListener('load', _self.funPlatformInfo);
-			delete _self['funPlatformInfo'];
-			_isApple = null;
-		};
-		_self.addEventListener('load', _self.funPlatformInfo);
-		_routeHTML = null;
-	//}
+	var _routeHTML = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, userName + '/operators/' + parameters.uri + '/index.html');
+	_self = Ti.UI.createWebView({
+		url : _routeHTML.nativePath,
+		width : 0,
+		height : 0,
+		top : 0,
+		left : 0,
+		flag : true,
+		visible : false
+	});
+	_self.funPlatformInfo = function funPlatformInfo() {
+		_self.evalJS("MashupPlatform.setPlatformInfo(" + idOperator + "," + "'operator'" + ");");
+	};
+	_self.clearObject = function clearObject() {
+		_self.removeEventListener('load', _self.funPlatformInfo);
+		delete _self['funPlatformInfo'];
+		_isApple = null;
+	};
+	_self.addEventListener('load', _self.funPlatformInfo);
+	_routeHTML = null;
 	return _self;
 }
 
